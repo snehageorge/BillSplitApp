25ab5ac63a55349b3c4e2551e73900bcb86369b0
Sneha <snehavg94@gmail.com>
2025-04-09 19:13:34 +0200

    Initial Commit

---
+++ BillSplitApp/BillSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitAppApp.swift
+//  BillSplitApp
+//
+//  Created by Sneha on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
